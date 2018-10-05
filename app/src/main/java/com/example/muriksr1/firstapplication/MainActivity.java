79ad6d6705d718e04e6b4d69dc7bcd41823d463c
alalawi <alalawiksa@gmail.com>
2018-10-05 16:41:01 +0530

    added registration & home screen

--- app/src/main/java/com/example/muriksr1/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/muriksr1/firstapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,28 +19,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
  protected void onCreate(@Nullable Bundle savedInstanceState) {
   super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main);
-  //Button button = new Button(this);
-  submit = (Button) findViewById(R.id.khazina);
-     username= (EditText)findViewById(R.id.user_name);
+        setContentView(R.layout.activity_main);
+        submit = (Button) findViewById(R.id.submit);
+        username= (EditText)findViewById(R.id.user_name);
         password= (EditText)findViewById(R.id.password);
+        TextView register= (TextView) findViewById(R.id.register);
+        submit.setOnClickListener(this);
+        register.setOnClickListener(this);
 
-
-  submit.setOnClickListener(this);
  }
 
     @Override
     public void onClick(View view) {
-        if (username.getText().toString().equals("user") && password.getText().toString().equals("123")) {
-            Toast.makeText(MainActivity.this, "login sucessfull", Toast.LENGTH_LONG).show();
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.register:
+                intent= new Intent(this, Main2Activity.class);
+                finish();
+                startActivity(intent);
+                break;
+            case R.id.submit:
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("123")) {
+                    Toast.makeText(MainActivity.this, "login sucessfull", Toast.LENGTH_LONG).show();
 
-            Intent intent= new Intent(this, Main2Activity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_LONG).show();
+                    intent= new Intent(this, Main3Activity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_LONG).show();
+                }
+                break;
         }
 
+
     }
+
+
 
  /*
     private static EditText username;
